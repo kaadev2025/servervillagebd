5219d6ef302749a25c101de1bb6b2ad196b6a639
kaadev2025 <kaadev2025@gmail.com>
2026-01-19 10:22:26 -0300

    se agrega ultimo pago de cuota

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -6,20 +6,25 @@ router.get("/", async (req, res) => {
   const { cli_codigo, cli_nombre } = req.query;
 
   let sql = `
-    SELECT cli_codigo, cli_nombre, cli_ruc, cli_zona, cli_codvdd, CLI_UPDATE
-    FROM cliente
+    SELECT c.cli_codigo, c.cli_nombre, c.cli_ruc, c.cli_zona, c.cli_codvdd, c.CLI_UPDATE, v.ultima_fecha_pago
+    FROM cliente c
+    LEFT JOIN (
+      SELECT ven_codcli, DATE_FORMAT(MAX(ven_fecha), '%d-%m-%Y') AS ultima_fecha_pago
+      FROM venta
+      GROUP BY ven_codcli
+    ) v ON v.ven_codcli = c.cli_codigo
     WHERE 1 = 1
   `;
 
   const params = [];
 
   if (cli_codigo) {
-    sql += " AND (cli_codigo = ? OR CAST(cli_codigo AS UNSIGNED) = ?)";
+    sql += " AND (c.cli_codigo = ? OR CAST(c.cli_codigo AS UNSIGNED) = ?)";
     params.push(cli_codigo, cli_codigo);
   }
 
   if (cli_nombre) {
-    sql += " AND cli_nombre LIKE ?";
+    sql += " AND c.cli_nombre LIKE ?";
     params.push(`%${cli_nombre}%`);
   }
 
@@ -35,9 +40,14 @@ router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   const sql = `
-    SELECT cli_codigo, cli_nombre, cli_ruc, cli_zona, cli_codvdd, CLI_UPDATE
-    FROM cliente
-    WHERE cli_codigo = ? OR CAST(cli_codigo AS UNSIGNED) = ?
+    SELECT c.cli_codigo, c.cli_nombre, c.cli_ruc, c.cli_zona, c.cli_codvdd, c.CLI_UPDATE, v.ultima_fecha_pago
+    FROM cliente c
+    LEFT JOIN (
+      SELECT ven_codcli, DATE_FORMAT(MAX(ven_fecha), '%d-%m-%Y') AS ultima_fecha_pago
+      FROM venta
+      GROUP BY ven_codcli
+    ) v ON v.ven_codcli = c.cli_codigo
+    WHERE c.cli_codigo = ? OR CAST(c.cli_codigo AS UNSIGNED) = ?
   `;
 
   try {
